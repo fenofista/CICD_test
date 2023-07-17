6154fbb692977dad6daffab535a4382cf46c78c2
Tony <s960068sss@gmail.com>
2023-07-17 13:54:53 +0800

    add model inference

---
+++ RF_inference.py
@@ -0,0 +1,24 @@
+import pickle
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+# Load the Random Forest model from the pkl file
+with open('TCHC_CV_model/RF_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Load the testing data from the CSV file
+test_data = pd.read_csv('T_for_AINA_2022.csv')
+
+# Extract the features from the testing data
+X_test = test_data.drop('CV', axis=1)  # Assuming 'label' column is the target variable
+
+# Make predictions on the testing data
+y_pred = model.predict(X_test)
+
+# Extract the actual labels from the testing data
+y_true = test_data['CV']
+
+# Compute the accuracy of the model
+accuracy = accuracy_score(y_true, y_pred)
+
+print("Accuracy:", accuracy)
---
+++ XGB_inference.py
@@ -0,0 +1,26 @@
+import xgboost as xgb
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+# Load the XGBoost model from the bst file
+model = xgb.Booster()
+model.load_model('TCHC_CV_model/XGB_model.bst')
+
+# Load the testing data from the CSV file
+test_data = pd.read_csv('T_for_AINA_2022.csv')
+
+# Extract the features from the testing data
+X_test = test_data.drop('CV', axis=1)  # Assuming 'label' column is the target variable
+
+# Make predictions on the testing data
+y_pred = model.predict(xgb.DMatrix(X_test))
+
+# Convert the predicted values to binary labels (if necessary)
+y_pred = [1 if pred > 0.5 else 0 for pred in y_pred]
+
+# Extract the actual labels from the testing data
+y_true = test_data['CV']
+
+# Compute the accuracy of the model
+accuracy = accuracy_score(y_true, y_pred)
+print("Accuracy:", accuracy)
